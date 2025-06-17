72f2eef3f19bdc51075b10152e0f5812ccdb1753
Naman Thakur <devnamanthakur853@gmail.com>
2025-06-17 16:44:25 +0530

    first commit

--- stack.c
+++ stack.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 20
+
+typedef struct Stack{
+    int a[N];
+    int top;
+}stack;
+
+void push(stack*s,int x){
+    if(s->top==N-1){
+        printf("Stack Overflow");
+    }
+}
