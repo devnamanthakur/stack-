60a52d363f0d57cbbf4e0eb0251a37f03222552a
Naman Thakur <devnamanthakur853@gmail.com>
2025-06-20 16:45:22 +0530

    All features added

--- stack.c
+++ stack.c
@@ -13,4 +13,73 @@ void push(stack*s,int x){
     if(s->top==N-1){
         printf("Stack Overflow");
     }
+    else{
+        s->top++;
+        s->a[s->top]=x;
+    }
 }
+
+void pop(stack*s){
+    int x;
+    if(s->top==-1){
+        printf("Stack underflow");
+    }
+    else{
+       x= s->a[s->top];
+        s->top--;
+    }
+
+}
+    void display(stack*s){
+        printf("[  ");
+        for(int i=s->top;i>=0;i--){
+            printf("%d ", s->a[i]);
+        }
+        printf(" ]");
+    }
+    void peek(stack*s){
+        printf("\n%d",s->a[s->top]);
+    }
+
+
+    int main(){
+        int ch,ele;
+        stack s;
+        s.top=-1;
+        while(1){
+        printf("\n");
+        printf("\n MENU \n");
+        printf("------------------\n");
+        printf(" 1.Push \n 2.Pop \n 3. Display \n 4.Peek\n 5.Exit \n");
+        printf("------------------\n");
+        printf("\n");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1: 
+            printf("Enter your element: ");
+            scanf("%d",&ele);
+            push(&s,ele);
+            break;
+
+            case 2:
+            pop(&s);
+            break;
+
+            case 3:
+            display(&s);
+            break;
+
+            case 4:
+            peek(&s);
+            break;
+
+            case 5:
+            break;
+            
+            default:
+            printf("Invalid choice");
+        
+        }
+
+    }
+    }
